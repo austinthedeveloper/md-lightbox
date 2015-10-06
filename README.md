c56bc2e0c5cef396bad6c8311e27aa7bbda5ea84
Austin Stewart <astewart@dcodit08.local>
2015-10-06 11:30:55 -0500

    Added the lightbox code

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # md-lightbox
 A lightbox directive for Angular Material
+
+##Demo
+[Plunkr](http://embed.plnkr.co/wbfUWDZQAfgVPwFtOZFC)
+
+##Requirements
+* Angular Material
+* Angular Material Icons (For the close button)
+
+##Use
+* Place `md-lightbox` in an image and it will start working
+* Adds the class `image-click` so the pointer cursor will show
+
+```
+<img ng-src="{{image}}" md-lightbox md-lightbox-title="TITLE">
+
+<img src="http://google.com/image.jpg" md-lightbox md-lightbox-title="TITLE">
+```
+
+###Fields
+* `md-lightbox`: Required
+* `md-lightbox-title`: Adds a title to the bar. Not Required
+
+##Files
+* Directive: `mdLightbox.js`
+* Partial: `lightbox.html`
---
+++ mdLightbox.js
@@ -0,0 +1,37 @@
+app.directive('mdLightbox', ['$mdDialog', function($mdDialog){
+	return {
+		link: function($scope, elem, attrs){
+
+			elem.addClass('image-click');
+
+		    elem.on('click',function(){		
+		    	var image = attrs.src;
+		    	var title = attrs.mdLightboxTitle;
+		    	showLightboxModal(image, title);
+				
+			});
+
+			//Lightbox Modal
+		    function showLightboxModal(image, title) {
+		        var confirm = $mdDialog.confirm({
+		            templateUrl: 'lightbox.html',
+		            clickOutsideToClose: true,
+		            controller: lightboxController
+		        });
+
+		        $mdDialog.show(confirm);
+
+		        function lightboxController($scope, $mdDialog) {
+		        	$scope.image = image;
+		        	$scope.title = title;
+
+		            $scope.cancel = function() {
+		                $mdDialog.cancel();
+		            };
+
+		        }
+
+		    }			
+		}
+	}
+}]);
